da6c1b12df7d3d284bf0289e1172d07717b24f59
Irene W <i.lalinw@gmail.com>
2021-08-21 05:00:04 -0700

    WIP: populating "plan_to_watch" in summary from API response

--- src/Components/FetchList.js
+++ src/Components/FetchList.js
@@ -5,7 +5,11 @@ import firebase from '../firebase';
 class FetchList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      user: "Irene",
+      usernameMAL: "zaphriz"
+    };
+    this.getListEndPointMAL = this.getListEndPointMAL.bind(this)
   }
   
   componentDidMount() {
@@ -21,10 +25,60 @@ class FetchList extends React.Component {
     console.log("fetching from MAL...");
     // Jikan API endpoint: https://api.jikan.moe/v3/
     //fetch user's watch list 
-    fetch("https://api.jikan.moe/v3/user/zaphriz/animelist/onhold")
+    var endpointMAL = this.getListEndPointMAL(this.state.usernameMAL, "onhold");
+    //console.log(endpointMAL);
+    fetch(endpointMAL)
       .then(res => res.json())
       .then((data) => {
         console.log(data);
+        
+        //clear the MAL list of IDs 
+        usersRef.doc("user1").update({
+          myanimelist: firebase.firestore.FieldValue.delete()
+        })
+
+        for (var i = 0; i < data.anime.length; i++) {
+          //console.log(data.anime[0]);
+
+          var thisAnime = data.anime[i];
+          usersRef.doc("user1").update({
+            myanimelist: firebase.firestore.FieldValue.arrayUnion(thisAnime.mal_id)
+          })
+          
+          //add to "all_references"
+          //should add check for exising document
+          // Firestore document ID must be a STRING!
+          summaryMAL.collection("all_references").doc(thisAnime.mal_id.toString())
+            .set({
+              title: thisAnime.title,
+              episodes: thisAnime.total_episodes,
+              image: thisAnime.image_url,
+              link: thisAnime.url
+          });
+          
+
+          // summaryMAL.collection("plan_to_watch").doc(thisAnime.mal_id.toString())
+          //   .get()
+          //   .then((doc) => {
+          //     console.log("updating plan_to_watch " + doc.exists);
+
+          //     if (doc.exists) {
+          //       summaryMAL.collection("plan_to_watch").doc(thisAnime.mal_id.toString()).update({
+          //         common_users: firebase.firestore.FieldValue.arrayUnion(this.state.user),
+          //         occurrences: firebase.firestore.FieldValue.increment(1)
+          //       });
+          //     } else {
+          //       console.log("inside the else");
+          //       summaryMAL.collection("plan_to_watch").doc(thisAnime.mal_id.toString()).set({
+          //         common_users: [this.state.user],
+          //         occurrences: 1
+          //       }).then(() => {console.log("set completed")});
+          //     }
+             
+          // });
+        }
+
+        
 
         //save data to firestore  @usersRef.doc("user1").collection("myanimelist")
         // data.anime array
@@ -43,8 +97,6 @@ class FetchList extends React.Component {
         // exceptions from actual bugs in components.
         (error) => {
           this.setState({
-            isLoaded: true,
-            error
           });
         }
       )
@@ -52,6 +104,9 @@ class FetchList extends React.Component {
     //save data to firestore
   }
   
+  getListEndPointMAL(usernameMAL, listTypeMAL) {
+    return "https://api.jikan.moe/v3/user/" + usernameMAL + "/animelist/" + listTypeMAL;
+  }
 
   render() {
     return (
