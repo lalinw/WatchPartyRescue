dfb99d3f47b3d7f3f2ff58472255935f461b12b1
Irene W <i.lalinw@gmail.com>
2021-09-16 20:22:22 -0700

    WIP

--- src/Components/Session.js
+++ src/Components/Session.js
@@ -54,10 +54,7 @@ class Session extends React.Component {
 
   handleSessionNameUpdate(event) {
     event.preventDefault();
-    this.setState(state => ({ 
-      editMode: false,
-      sessionName: state.tempSessionName
-    }));
+    
 
     // this.props.loadingGIF(true);
 
@@ -66,6 +63,10 @@ class Session extends React.Component {
     }).then(() => {
       // this.props.loadingGIF(false);
       console.log("Session Name successfully updated!");
+      this.setState(state => ({ 
+        editMode: false,
+        sessionName: state.tempSessionName
+      }));
 
     }).catch((error) => {
       // The document probably doesn't exist.
